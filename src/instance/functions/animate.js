c6505a75e5a2fae9353c1959fca0b6d0a24db627
J.L. Makes <julianlloyd@gmail.com>
2017-01-31 18:50:30 +0100

    add reveal reset sequence logic

--- src/instance/functions/animate.js
+++ src/instance/functions/animate.js
@@ -28,13 +28,13 @@ export default function animate (element) {
 
 	} else {
 		if (!isElementVisible.call(this, element) && element.visible && element.config.reset) {
-			// if (sequence) {
-			// 	if (sequence.head.index === element.sequence.index) {
-			// 		sequence.head.index++
-			// 	} else if (sequence.tail.index === element.sequence.index) {
-			// 		sequence.tail.index--
-			// 	} else return
-			// }
+			if (sequence) {
+				if (sequence.head.index === element.sequence.index) {
+					sequence.head.index++
+				} else if (sequence.tail.index === element.sequence.index) {
+					sequence.tail.index--
+				} else return
+			}
 
 			styles.push(element.styles.opacity.generated)
 			styles.push(element.styles.transform.generated.initial)
